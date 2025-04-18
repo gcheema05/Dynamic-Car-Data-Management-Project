ed224d01ffed986f082b0dc384642e226eb05f45
Gurbaaz Cheema <154286899+gcheema05@users.noreply.github.com>
2025-04-18 18:33:00 -0400

    Update headerA3.h

--- include/headerA3.h
+++ include/headerA3.h
@@ -1,18 +1,6 @@
 /*
   Student Name: Gurbaaz Cheema
-  Student ID: 1278175
   Due Date: 04 05, 2024
-  Course: CIS*2500
-
-  I have exclusive control over this submission via my password.
-  By including this header comment, I certify that:
-    1) I have read and understood the policy on academic integrity.
-    2) I have completed Moodle's module on academic integrity.
-    3) I have achieved at least 80% on the academic integrity quiz
-  I assert that this work is my own. I have appropriate acknowledged
-  any and all material that I have used, be it directly quoted or
-  paraphrased. Furthermore, I certify that this assignment was written
-  by me in its entirety.
 */
 
 #include <stdio.h>
